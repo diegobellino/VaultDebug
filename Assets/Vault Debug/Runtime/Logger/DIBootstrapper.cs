a4f873af3c547cf671b5c668db9f63643b991ec2
Diego Bellino <dbellinog@gmail.com>
2025-02-23 11:54:03 +0100

    chore: format

--- Assets/Vault Debug/Runtime/Logger/DIBootstrapper.cs
+++ Assets/Vault Debug/Runtime/Logger/DIBootstrapper.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 namespace VaultDebug.Runtime.Logger
 {
+    /// <summary>
+    /// Lightweight DI Bootstrapper to help resolve dependencies using a basic DIContainer. It's meant to be accessible from both editor scripts and runtime.
+    /// TODO: Substitute for something more extensible in the future, like UnityContainer
+    /// </summary>
 #if UNITY_EDITOR
     [InitializeOnLoad]
 #endif
@@ -21,7 +25,9 @@ namespace VaultDebug.Runtime.Logger
         public static void Initialize()
         {
             if (_container != null)
+            {
                 return;
+            }
 
             _container = new DIContainer();
 
